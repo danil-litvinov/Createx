d8212fdd81a32d0211336fff47e78023aa4e58e7
danil-litvinov <111233175+danil-litvinov@users.noreply.github.com>
2023-05-14 02:29:25 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -11,4 +11,20 @@ $(function () {
     $(this).addClass('directions__filter-btn--active')
   })
 
+  $('.team__slider').slick({
+    arrows: false,
+    slidesToShow: 4,
+    draggable: false,
+    waitForAnimate: false,
+  });
+
+  $('.team__arrow-prev').on('click', function (e) {
+    e.preventDefault()
+    $('.team__slider').slick('slickPrev')
+  })
+
+  $('.team__arrow-next').on('click', function (e) {
+    e.preventDefault()
+    $('.team__slider').slick('slickNext')
+  })
 })
